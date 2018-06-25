2774c068b6d9fd118c566bf1ee646405f2475004
Hongli183 <261930323@qq.com>
2018-06-25 19:18:54 +0800

    add header file

---
+++ TSUITableViewExtension/TSTableViewExtensionFile/TSTableViewEmptyTool.h
@@ -0,0 +1,15 @@
+//
+//  TSTableViewEmptyTool.h
+//  TSUITableViewExtension
+//
+//  Created by 洪利 on 2018/6/25.
+//  Copyright © 2018年 洪利. All rights reserved.
+//
+
+#ifndef TSTableViewEmptyTool_h
+#define TSTableViewEmptyTool_h
+#import "MJ_TableView_Empty_DefaultView.h"
+#import "MJ_TableView_EmptyView_Protocol.h"
+#import "UITableView+EmptyControl.h"
+
+#endif /* TSTableViewEmptyTool_h */
